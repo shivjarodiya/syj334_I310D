07f06a48fb48e2143d60a2825f7b29a839f20a81
shivjarodiya <syjarodiya@gmail.com>
2025-04-09 13:31:05 -0500

    Added more area calculations

--- area_of_a_circle.py
+++ area_of_a_circle.py
@@ -10,3 +10,11 @@ print(f"The area of circle with radius {radius1} is: {area1}")
 radius2 = 40
 area2 = compute_area_of_circle(radius2)
 print(f"The area of circle with radius {radius2} is: {area2}")
+
+radius3 = 50
+area1 = compute_area_of_circle(radius1)
+print(f"The area of circle with radius {radius1} is: {area1}")
+
+radius4 = 60
+area2 = compute_area_of_circle(radius2)
+print(f"The area of circle with radius {radius2} is: {area2}")
